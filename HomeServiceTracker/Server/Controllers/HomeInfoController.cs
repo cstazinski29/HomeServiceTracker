fc463c248acf56535bc6a4c13e72a6eb2d8c729f
cstazinski29 <cstazinski@gmail.com>
2022-07-07 19:51:31 -0400

    created serviceItem get & post requests in controller

--- HomeServiceTracker/Server/Controllers/HomeInfoController.cs
+++ HomeServiceTracker/Server/Controllers/HomeInfoController.cs
@@ -1,4 +1,5 @@
 ﻿using HomeServiceTracker.Server.Services.HomeInfo;
+using HomeServiceTracker.Shared.Models.HomeInfo;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -7,7 +8,8 @@ namespace HomeServiceTracker.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class HomeInfoController : ControllerBase
+    // public class HomeInfoController : ControllerBase
+    public class HomeInfoController : Controller
     {
         private readonly IHomeInfoService _homeInfoService;
         public HomeInfoController(IHomeInfoService homeInfoService)
@@ -30,6 +32,41 @@ namespace HomeServiceTracker.Server.Controllers
                 return false;
             _homeInfoService.SetUserId(userId);
             return true;
+        }
+
+        [HttpGet]
+        public async Task<List<HomeInfoListItem>> Index()
+        {
+            if (!SetUserIdInService()) return new List<HomeInfoListItem>();
+            var homeInfo = await _homeInfoService.GetAllHomeInfoAsync();
+            return homeInfo.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Note(int id)
+        {
+            if (!SetUserIdInService()) return Unauthorized();
+
+            var homeInfo = await _homeInfoService.GetHomeInfoByIdAsync(id);
+            if (homeInfo == null)
+                return NotFound();
+
+            return Ok(Note);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(HomeInfoCreate model)
+        {
+            if (model == null) return BadRequest();
+            if (!SetUserIdInService()) return Unauthorized();
+
+            bool wasSuccessful = await _homeInfoService.CreateHomeInfoAsync(model);
+
+            if (wasSuccessful) return Ok();
+                else return UnprocessableEntity();
+
+        // [HttpPut]
+        // [HttpDelete]
         }
     }
 }
